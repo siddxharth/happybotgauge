c7b4079ead29c565257ef1ba2a4b5357969fb00b
Siddharth Khatri <siddxharth@gmail.com>
2024-06-11 01:11:21 +0530

    Update terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -52,25 +52,6 @@ resource "aws_eks_cluster" "eks_cluster" {
   ]
 }
 
-# EKS Node Group
-resource "aws_eks_node_group" "node_group" {
-  cluster_name  = aws_eks_cluster.eks_cluster.name
-  node_role_arn = aws_iam_role.node_group_role.arn
-  subnet_ids    = aws_subnet.private_subnets.*.id
-
-  scaling_config {
-    desired_size = 2
-    max_size     = 4
-    min_size     = 1
-  }
-
-  instance_types = ["t3.medium"]
-
-  tags = {
-    Name = "node-group"
-  }
-}
-
 # IAM Roles and Policies
 resource "aws_iam_role" "eks_cluster_role" {
   name = "eks-cluster"
@@ -101,36 +82,6 @@ resource "aws_iam_role_policy_attachment" "eks_service_policy" {
   role       = aws_iam_role.eks_cluster_role.name
 }
 
-resource "aws_iam_role" "node_group_role" {
-  name = "eks-node-group-role-2"
-
-  assume_role_policy = jsonencode({
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-    }]
-    Version = "2012-10-17"
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "node_group_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.node_group_role.name
-}
-
-resource "aws_iam_role_policy_attachment" "cni_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.node_group_role.name
-}
-
-resource "aws_iam_role_policy_attachment" "ecr_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.node_group_role.name
-}
-
 # Security Groups
 resource "aws_security_group" "eks_cluster_sg" {
   name        = "eks-cluster-sg"
@@ -155,31 +106,5 @@ resource "aws_security_group" "eks_cluster_sg" {
 
   tags = {
     Name = "eks-cluster-sg"
-  }
-}
-
-resource "aws_security_group" "node_group_sg" {
-  name        = "node-group-sg"
-  description = "Security group for EKS node group"
-  vpc_id      = aws_vpc.eks_vpc.id
-
-  ingress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    security_groups = [aws_security_group.eks_cluster_sg.id]
-    description     = "Allow all traffic from the EKS cluster security group"
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Allow all outbound traffic"
-  }
-
-  tags = {
-    Name = "node-group-sg"
   }
 }
